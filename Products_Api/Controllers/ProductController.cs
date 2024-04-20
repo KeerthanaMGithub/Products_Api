853c3134cc4858c9cd3a280afe9ba920a8b8ea88
user <user@DESKTOP-U9BUE2V>
2024-04-20 19:28:13 +0530

    Add project files.

---
+++ Products_Api/Controllers/ProductController.cs
@@ -0,0 +1,111 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ApiExplorer;
+using Newtonsoft.Json;
+using Products_Api.Models;
+using System.Data;
+using System.Security.Cryptography;
+using System.Xml.Linq;
+using System.Xml;
+using System.Collections.Generic;
+using System;
+
+namespace Products_Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+       
+
+        [HttpGet()]
+        [Route("Products")]
+        public async Task<IActionResult> Products()
+        {
+            List<ProductData> products1 = new List<ProductData>();
+            products1.Add(new ProductData { id = 1, name = "Apple", description = "Apple_Description", price = 120 });
+            return Ok(products1);
+        }
+
+        [HttpPost()]
+        [Route("Products")]
+        public async Task<IActionResult> Product(int pId,string pName,string pDescription,decimal pPrice,string jsonstr)
+        {
+            
+            try
+            {
+                List<ProductData> ProductsWithKey = Newtonsoft.Json.JsonConvert.DeserializeObject<List<ProductData>>(jsonstr);
+
+                List<ProductData> products1 = new List<ProductData>();
+                products1.Add(new ProductData { id = pId+1, name = pName, description = pDescription, price = pPrice });
+
+                ProductsWithKey.AddRange(products1);
+
+
+                string result = JsonConvert.SerializeObject(ProductsWithKey);
+                return Ok(result);
+
+
+            }
+            catch (Exception ex)
+            {
+
+            }
+
+            return Ok();
+
+        }
+
+        [HttpPut()]
+        [Route("Products")]
+        public async Task<IActionResult> Products_Update(int pId, string pName, string pDescription, decimal pPrice, string jsonstr)
+        {
+            List<ProductData> products1 = new List<ProductData>();
+            try
+            {
+                List<ProductData> ProductsWithKey = JsonConvert.DeserializeObject<List<ProductData>>(jsonstr);
+                for (var i = 0; i < ProductsWithKey.Count; i++)
+                {
+                    if(i==pId)
+                    {
+                        ProductsWithKey[i].name = pName;
+                        ProductsWithKey[i].description = pDescription;
+                        ProductsWithKey[i].price = pPrice;
+                    }
+                }
+                string result = JsonConvert.SerializeObject(ProductsWithKey);
+                return Ok(result);
+            }
+            catch(Exception ex)
+            {
+
+                return Ok(products1);
+            }
+
+
+                
+            
+
+        }
+
+        [HttpDelete()]
+        [Route("Products")]
+        public async Task<IActionResult> Products_Delete(int pId, string jsonstr)
+        {
+            
+            List<ProductData> ProductsWithKey = JsonConvert.DeserializeObject<List<ProductData>>(jsonstr);
+            ProductsWithKey.RemoveAt(pId);
+
+            for (var i = 0; i < ProductsWithKey.Count; i++)
+            {   
+                ProductsWithKey[i].id = i+1;                    
+            }
+
+
+
+            return Ok(ProductsWithKey);
+        }
+
+
+    }
+}
---
+++ Products_Api/Models/ProductData.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Products_Api.Models
+{
+    public class ProductData
+    {
+        [Key]
+        public int id { get; set; }
+        [Required]
+        public string name { get; set; } = "";
+        [Required]
+        public string description { get; set; } = "";
+        [Required]
+        public decimal price { get; set; } = 0;
+    }
+}
