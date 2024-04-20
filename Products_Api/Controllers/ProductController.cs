5e139b69885e8a0421a60eaf2bb3e198e968c1cc
keerthana <keerthanam062001@gmail.com>
2024-04-20 20:11:06 +0530

    Commit

--- Products_Api/Controllers/ProductController.cs
+++ Products_Api/Controllers/ProductController.cs
@@ -9,7 +9,7 @@ using System.Xml.Linq;
 using System.Xml;
 using System.Collections.Generic;
 using System;
-
+//Created by Keerthana M on 20/04/2024
 namespace Products_Api.Controllers
 {
     [Route("api/[controller]")]
@@ -17,7 +17,7 @@ namespace Products_Api.Controllers
     public class ProductController : ControllerBase
     {
        
-
+        //To Get Products List
         [HttpGet()]
         [Route("Products")]
         public async Task<IActionResult> Products()
@@ -27,35 +27,27 @@ namespace Products_Api.Controllers
             return Ok(products1);
         }
 
+        //To Post Products List
         [HttpPost()]
         [Route("Products")]
         public async Task<IActionResult> Product(int pId,string pName,string pDescription,decimal pPrice,string jsonstr)
         {
-            
+            List<ProductData> ProductsWithKey = Newtonsoft.Json.JsonConvert.DeserializeObject<List<ProductData>>(jsonstr);
+            List<ProductData> products1 = new List<ProductData>();
             try
-            {
-                List<ProductData> ProductsWithKey = Newtonsoft.Json.JsonConvert.DeserializeObject<List<ProductData>>(jsonstr);
-
-                List<ProductData> products1 = new List<ProductData>();
+            {                
                 products1.Add(new ProductData { id = pId+1, name = pName, description = pDescription, price = pPrice });
-
                 ProductsWithKey.AddRange(products1);
-
-
                 string result = JsonConvert.SerializeObject(ProductsWithKey);
                 return Ok(result);
-
-
             }
             catch (Exception ex)
             {
-
-            }
-
-            return Ok();
-
+                return Ok(ProductsWithKey);
+            }         
         }
 
+        //To Update Products List
         [HttpPut()]
         [Route("Products")]
         public async Task<IActionResult> Products_Update(int pId, string pName, string pDescription, decimal pPrice, string jsonstr)
@@ -78,31 +70,21 @@ namespace Products_Api.Controllers
             }
             catch(Exception ex)
             {
-
                 return Ok(products1);
-            }
-
-
-                
-            
-
+            }                           
         }
 
+        //To Delete Products List
         [HttpDelete()]
         [Route("Products")]
         public async Task<IActionResult> Products_Delete(int pId, string jsonstr)
-        {
-            
+        {            
             List<ProductData> ProductsWithKey = JsonConvert.DeserializeObject<List<ProductData>>(jsonstr);
             ProductsWithKey.RemoveAt(pId);
-
             for (var i = 0; i < ProductsWithKey.Count; i++)
             {   
                 ProductsWithKey[i].id = i+1;                    
             }
-
-
-
             return Ok(ProductsWithKey);
         }
 
